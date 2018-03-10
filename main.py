0b7cd6d81817350f54c32279dee5bbbc42ea2726
Jonathan Li <jonathanliworld@gmail.com>
2018-03-10 15:35:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 import webbrowser
 
 while True:
-    pages = random.choice(["trollface.dk", "www.youtube.com/watch?v=2VXacYLcjGA",\  # Websites to open
-    "media.giphy.com/media/eVy46EWyclTIA/giphy.gif"])
+    pages = random.choice(["trollface.dk", "www.youtube.com/watch?v=2VXacYLcjGA",\
+    "media.giphy.com/media/eVy46EWyclTIA/giphy.gif"])  # Websites to open
     visit = "https://{}".format(pages)
     timeWait = random.randrange(1, 3)  # Delay
     time.sleep(timeWait)
